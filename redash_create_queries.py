e2dbb697cbf129cb7808fce7032164e01f50adf1
Andi Darmawan <92540957+andidar83@users.noreply.github.com>
2024-04-29 17:40:27 +0700

    Update redash_create_queries.py

--- redash_create_queries.py
+++ redash_create_queries.py
@@ -1,23 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 29 15:14:13 2024
-
-@author: NXP
-"""
-
 import requests
 import json
 
 
 payload = {
-           "query":'select * from orders limit 5', ## the select query
+           "query":'select * from xxx', ## the select query
            "name":"test api new query",
            "data_source_id":1, ## can be determined from the /api/data_sources end point
            "schedule":None,
            "options":{"parameters":[]}
            }
-res = requests.post('https://redash-id.ninjavan.co/' + '/api/queries', 
-                    headers = {'Authorization':'Poqwen8BjW4zjLHOzpmSlXDnm7TSrV5DzevDv6eD'},
+res = requests.post('your redash domain' + '/api/queries', 
+                    headers = {'Authorization':'your api'},
                     json=payload)
 
 print(res)  
